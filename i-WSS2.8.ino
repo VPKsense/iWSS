88685c061db1c4e06b9f9663437c8b2844255c2d
VPKsense <89151329+VPKsense@users.noreply.github.com>
2022-04-15 21:14:36 +0530

    i-WSS2.8_Sense_OS_v1.6.3
    ---Sense OS v1.6--- <25/04/2020>
    >Disconnection indicator LED
    >Blynk-cloud.com IP address resolved for connection
    >No internet access indicator
    >Buzzer sound for disconnection
    
    ---Sense OS v1.6.2--- <26/04/2020>
    >Buzzer sound for reconnection
    >Time display moved from ping function to SSTcheck for updating display time each minute
    >Other optimizations
    
    ---Sense OS v1.6.3--- <23/06/2020>
    >Reduced 5 mins on sunset time for last light
    >Goodnight function modified to compensate server disconnection

--- i-WSS2.8.ino
+++ i-WSS2.8.ino
@@ -39,7 +39,8 @@ int Gate,Sitout;
 int RAR,RARset;
 int WBH,WBHset,WBHsel;
 int GNset,GN;
-int timeID;
+int conbuz=0;
+int hr,mt;
 ////////////////////////
 
 WidgetRTC rtc;
@@ -126,27 +127,31 @@ BLYNK_WRITE(SSTp)//SST store value
 
 BLYNK_WRITE(SSTimeCheck)//SST time store value
 {
-SSTime=param.asInt();  
+SSTime= Home.sunset(year(), month(), day(), false)+30+15;
 }
 
-void SSTCheck()//SST main
+void SSTCheck()//SST main & Time keeper
 {
-  CurTime= (hour()*60)+ minute();
-  if((CurTime>SSTime)&&(SSTcheck==5))
+  hr=hour();
+  mt=minute();
+  CurTime= (hr*60)+ mt;
+  Blynk.virtualWrite(Timep, String(hr)+ ":" + mt );
+  if((CurTime>=SSTime)&&(SSTcheck==5))
   {
    SSTmain(); 
    SSTcheck=7;
    Blynk.virtualWrite(SSTp,7);
   }
-  if((hour()<1)&&(SSTcheck==7))//get sunset time for new day
+  if((hr<1)&&(SSTcheck==7))//get sunset time for new day
   {
-    SSTime= Home.sunset(year(), month(), day(), false)+30+23;//30 for time zone and 23 for approx last light
+    SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
     SSTcheck=5;
+    char SSTime24[] = "00:00";
+    Dusk2Dawn::min2str(SSTime24, SSTime);
     Blynk.virtualWrite(SSTp,5);
-    Blynk.virtualWrite(SSTimeCheck,SSTime);//To get Sunset time on app
+    Blynk.virtualWrite(SSTimeCheck,SSTime24);//To get Sunset time on app
   }
 }
-
 ///////////////////////////////////////////////////////////////
 
 ////////////////////////Rain Alert Reporter/////////////////////////
@@ -156,7 +161,7 @@ BLYNK_WRITE(RARp)//RAR main
   RAR=param.asInt();
   if((RAR==5)&&(RARset==1))
   {
-    Blynk.notify("Looks like it's gonna rain at your home");
+    Blynk.notify("Looks like it's gonna rain at your home (time: "+ String(hour())+ ":" + String(minute()) + ")");
     for(int k=0;k<5;k++)
     {
         tone(D8,4000);
@@ -230,7 +235,7 @@ BLYNK_WRITE(WBHselp)//WBH selection
 
 ////////////////////////////////////////////////////////////////
 
-////////////
+
 ////////////Good Night///////////////////////////////
 
 BLYNK_WRITE(GNp)//GN main
@@ -240,6 +245,7 @@ BLYNK_WRITE(GNp)//GN main
  {Blynk.virtualWrite(Sitoutp,0);
   Blynk.notify("Good night! Sit out light has been turned OFF");
   digitalWrite(D5,HIGH);
+  GN=5;
  }
 }
 
@@ -255,8 +261,8 @@ BLYNK_WRITE(GNsetp)//GN switch
 BLYNK_WRITE(Pingp)
 {int ping=param.asInt();
 if(ping)
-{ Blynk.virtualWrite(Timep, String(hour())+ " : " + minute() );
-  Blynk.notify("Ping success!");
+{ 
+ Blynk.notify("Ping success!");
 }  
 }
   
@@ -268,24 +274,41 @@ BLYNK_CONNECTED()
   rtc.begin();
   Blynk.notify("I'm Online :-)");
   delay(5000);
-  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,SSTp,SSTsetp,SSTimeCheck,RARsetp,WBHsetp,WBHselp,GNsetp);
+  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,SSTp,SSTsetp,SSTimeCheck,RARsetp,WBHsetp,WBHselp,GNsetp,GNp);
   pstat++;
+  digitalWrite(D4,HIGH);
   }
 else{
-Blynk.notify("I've Reconnected to the server"); 
-Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,WBHsetp,WBHselp,GNsetp);}
+ tone(D8,4000);//Reconnection buzzer
+ delay(150);
+ noTone(D8);
+ delay(80);
+ tone(D8,4000);
+ delay(150);
+ noTone(D8);
+ delay(200);
+ tone(D8,4000);
+ delay(1500);
+ noTone(D8); 
+Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,WBHsetp,WBHselp,GNsetp,GNp);
+Blynk.notify("I've Reconnected to the server");
+digitalWrite(D4,HIGH);
+conbuz=0;
+    }
 }
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println();
-  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.5 for i-WSS-");
+  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.6.3 for i-WSS-");
   Serial.println("Booting up...");
-  Blynk.begin(auth, ssid, pass);
-  setSyncInterval(30 * 60);
+  pinMode(D4,OUTPUT);//Noconnection LED
+  digitalWrite(D4,LOW);
+  Blynk.begin(auth, ssid, pass,IPAddress(188,166,206,43),8080);
+  setSyncInterval(120 * 60);// 2 hr
   EEPROM.begin(3);  
-  timer.setInterval(5*60*1000, SSTCheck);
+  timer.setInterval(60*1000, SSTCheck);//1 minute
   pinMode(D3,OUTPUT);//gate
   pinMode(D5,OUTPUT);//sitout
   delay(2000);
@@ -293,6 +316,27 @@ void setup()
 
 void loop()
 {
-  Blynk.run();
   timer.run();
-}  
+  if (Blynk.connected()) 
+  {  
+    Blynk.run();
+  } 
+  else  
+  { 
+    digitalWrite(D4,LOW);
+    if(conbuz==0)
+    {
+        for(int i=0;i<5;i++)
+        {tone(D8,4000);
+        delay(150);
+        noTone(D8);
+        delay(80);
+        tone(D8,4000);
+        delay(150);
+        noTone(D8);
+        delay(500);}
+    conbuz=1;
+    }
+    Blynk.connect(); 
+  } 
+}
