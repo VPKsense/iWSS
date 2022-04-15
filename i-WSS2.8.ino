3ecb73e8c34e738c6ceb105a30a70c73462acf74
VPKsense <89151329+VPKsense@users.noreply.github.com>
2022-04-15 21:19:13 +0530

    i-WSS2.8_Sense_OS_v1.6.7
    ---Sense OS v1.6.7--- <11/03/2021>
    >Removed power restored notification and MSR LED
    >Edited MSR status to report only power interruption and Changed MSR status to BETA
    >Added switch to enable or disable reconnection buzzer sound and moved reconnection notification into it
    >Added Disconnection counter to count the no. of disconnections
    >Changed 'Im Online' power on notification to 'Im Ready' 
    >Removed EEPROM headerfile and setup function

--- i-WSS2.8.ino
+++ i-WSS2.8.ino
@@ -5,17 +5,16 @@
 #include <BlynkSimpleEsp8266.h>
 #include <TimeLib.h>
 #include <WidgetRTC.h>
-#include <EEPROM.h>
 
 char auth[] = "oKPdLYfuKUOkgUS6bhhNL43oetoQNBTj";
-char ssid[] = "2PoInT0";
+char ssid[] = "2PoInT1";
 char pass[] = "akhilesh";
 
 //////V pins Define//////
 #define Gatep V2
 #define Sitoutp V3
 #define MSRp V4
-#define MSRledp V5
+#define Buzsetp V5
 #define SSTp V6 
 #define SSTsetp V7
 #define RARp V8
@@ -29,6 +28,7 @@ char pass[] = "akhilesh";
 #define GNp V16
 #define GNsetp V17
 #define SSTimeCheck V18 
+#define DisCounterp V19
 ////////////////////////
 
 ///Support variables/////
@@ -41,6 +41,8 @@ int WBH,WBHset,WBHsel;
 int GNset,GN;
 int conbuz=0;
 int hr,mt;
+int conbuzstat;
+int DisCount=0;
 ////////////////////////
 
 WidgetRTC rtc;
@@ -69,35 +71,13 @@ else if(!Sitout)
 
 //////////////////////////////////////////////////////////////
 
-////////////////MAINS STATUS REPORTER (ALPHA)/////////////////
-
-WidgetLED led(MSRledp);
-
-void Powernotify()//MSR main function
-{
-  curpowerstat=EEPROM.read(1);
-  if(curpowerstat==1)
-  {
-  Blynk.notify("Mains Power has been restored");
-  curpowerstat=0;
-  EEPROM.write(1,curpowerstat);
-  EEPROM.commit();
-  led.on();
-  }
-  else if(curpowerstat==0)
-  {
-  Blynk.notify("Mains Power has been lost");
-  curpowerstat=1;
-  EEPROM.write(1,curpowerstat);
-  EEPROM.commit();
-  led.off();
-  }
-}
+////////////////MAINS STATUS REPORTER (BETA)//////////////////
 
 BLYNK_WRITE(MSRp)//Switch to enable Mains Status Reporter 
 { MSRs=param.asInt();
 if(MSRs)
-{Powernotify();}
+{Blynk.notify("Looks like Mains Power got interrupted");
+}
 }
 
 ///////////////////////////////////////////////////////////////
@@ -142,7 +122,7 @@ void SSTCheck()//SST main & Time keeper
    SSTcheck=7;
    Blynk.virtualWrite(SSTp,7);
   }
-  if((hr<1)&&(SSTcheck==7))//get sunset time for new day
+  if((hr<1)&&(SSTcheck==7))//get sunset time for NEW day
   {
     SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
     SSTcheck=5;
@@ -150,6 +130,8 @@ void SSTCheck()//SST main & Time keeper
     Dusk2Dawn::min2str(SSTime24, SSTime);
     Blynk.virtualWrite(SSTp,5);
     Blynk.virtualWrite(SSTimeCheck,SSTime24);//To get Sunset time on app
+    DisCount=0; //Reset disconnection counter to 0 at the start of new day
+    Blynk.virtualWrite(DisCounterp,DisCount);
   }
 }
 ///////////////////////////////////////////////////////////////
@@ -268,17 +250,30 @@ if(ping)
   
 //////////////////////////////
 
+//////////Disconection Buzzer+Counter///////////
+
+BLYNK_WRITE(Buzsetp){
+  conbuzstat=param.asInt();
+}
+
+BLYNK_WRITE(DisCounterp){
+  DisCount=param.asInt();
+}
+  
+///////////////////////////////////////////////
+
 BLYNK_CONNECTED()
 { if(pstat==0)
   {
   rtc.begin();
-  Blynk.notify("I'm Online :-)");
+  Blynk.notify("I'm ready :-)");
   delay(5000);
-  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,SSTp,SSTsetp,SSTimeCheck,RARsetp,WBHsetp,WBHselp,GNsetp,GNp);
+  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,SSTp,SSTsetp,SSTimeCheck,RARsetp,WBHsetp,WBHselp,GNsetp,GNp,Buzsetp,DisCounterp);
   pstat++;
   digitalWrite(D4,HIGH);
   }
 else{
+ if(conbuzstat==1){
  tone(D8,4000);//Reconnection buzzer
  delay(150);
  noTone(D8);
@@ -289,11 +284,13 @@ else{
  delay(200);
  tone(D8,4000);
  delay(1500);
- noTone(D8); 
-Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,WBHsetp,WBHselp,GNsetp,GNp);
-Blynk.notify("I've Reconnected");
+ noTone(D8);
+ Blynk.notify("I've Reconnected");} 
+Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,WBHsetp,WBHselp,GNsetp,GNp,Buzsetp);
 digitalWrite(D4,HIGH);
 conbuz=0;
+DisCount++;
+Blynk.virtualWrite(DisCounterp,DisCount); //Count no. of disconnections in  a day
     }
 }
 
@@ -301,13 +298,12 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println();
-  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.6.4 for i-WSS-");
+  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.6.7 for i-WSS-");
   Serial.println("Booting up...");
   pinMode(D4,OUTPUT);//Noconnection LED
   digitalWrite(D4,LOW);
   Blynk.begin(auth, ssid, pass,IPAddress(188,166,206,43),8080);
   setSyncInterval(120 * 60);// 2 hr
-  EEPROM.begin(3);  
   timer.setInterval(60*1000, SSTCheck);//1 minute
   pinMode(D3,OUTPUT);//gate
   pinMode(D5,OUTPUT);//sitout
@@ -324,7 +320,7 @@ void loop()
   else  
   { 
     digitalWrite(D4,LOW);
-    if(conbuz==0)
+    if((conbuz==0)&&(conbuzstat==1))
     {
         for(int i=0;i<5;i++)
         {tone(D8,4000);
