9413638d071840b75ba55963890bea9755f860f9
Vishnuprasad.K <89151329+VPKsense@users.noreply.github.com>
2022-10-17 18:11:15 +0530

    i-WSS2.8_Sense_OS_v1.8.4_OTA Update

--- i-WSS2.8.ino
+++ i-WSS2.8.ino
@@ -1,15 +1,19 @@
 #define BLYNK_PRINT Serial
 
+#define BLYNK_TEMPLATE_ID "###"
+#define BLYNK_DEVICE_NAME "iWSS"
+#define BLYNK_AUTH_TOKEN "###"
+
 #include <Dusk2Dawn.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <TimeLib.h>
 #include <WidgetRTC.h>
+#include <EEPROM.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-char auth[] = "###";
+char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "###";
 char pass[] = "###";
 
@@ -22,9 +26,9 @@ char pass[] = "###";
 #define SSTsetp V7
 #define RARp V8
 #define RARsetp V9
-#define WBHp V10
-#define WBHsetp V11
-#define WBHselp V12
+//#define WBHp V10
+//#define WBHsetp V11
+//#define WBHselp V12
 #define Buzztestp V13
 #define Pingp V14
 #define Timep V15
@@ -47,10 +51,10 @@ char pass[] = "###";
 ///Support variables/////
 int pstat=0; 
 int MSRs=0,curpowerstat;
-int CurTime,SSTset,SSTime,SSTcheck;
+int CurTime,SSTset,SSTime,SSTcheck=2;
 int Gate,Sitout;
 int RAR,RARset;
-int WBH,WBHset,WBHsel;
+//int WBH,WBHset,WBHsel;
 int GNset,GN;
 int GNTime=1305;//9:45 PM
 int conbuz=0;
@@ -60,6 +64,8 @@ int DisCount=0;
 int SRTime,SRTcount=0,CrowAlt=true;
 String Condition;
 int WeatherComp=0;
+int pflag=0,sflag=0;
+int Shr,Smin;
 ////////////////////////
 
 WidgetRTC rtc;
@@ -93,7 +99,7 @@ else if(!Sitout)
 BLYNK_WRITE(MSRp)//Switch to enable Mains Status Reporter 
 { MSRs=param.asInt();
 if(MSRs)
-{Blynk.notify("Looks like Mains Power got interrupted");
+{Blynk.logEvent("mains","Looks like Mains Power got interrupted");
 }
 }
 
@@ -114,11 +120,11 @@ BLYNK_WRITE(SSTp)//SST store value
 
 BLYNK_WRITE(SSTimeCheck)//SST time store value
 {
-SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
-if(WeatherComp==1)
-  SSTime-=10;
-else if(WeatherComp==2)
-  SSTime-=20;
+  SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
+  if(WeatherComp==1)
+    SSTime-=10;
+  else if(WeatherComp==2)
+    SSTime-=20;
 }
 
 BLYNK_WRITE(SRTp)//SRT count store
@@ -158,6 +164,10 @@ BLYNK_WRITE(WeaCon)//SST Weather condition compensation
     WeatherComp=0;
     Blynk.virtualWrite(WeaConp,WeatherComp);
   }
+  
+  EEPROM.write(4,SSTime/60);
+  EEPROM.write(5,SSTime%60);
+  EEPROM.commit();  
 }
 
 void SSTmain()//Switching function
@@ -167,56 +177,96 @@ void SSTmain()//Switching function
   Blynk.virtualWrite(Sitoutp,1);
   digitalWrite(D5,LOW);
   if(!WeatherComp)
-  Blynk.notify("Good Evening! Sit out light has been turned ON");
+  Blynk.logEvent("lights","Good Evening! Sit out light has been turned ON");
   else
-  Blynk.notify("Good Evening! Sit out light has been turned ON early due to "+Condition);
+  Blynk.logEvent("lights",String("Good Evening! Sit out light has been turned ON early due to ")+Condition);
   } 
 }
 
-void SSTCheck()//SST main & Time keeper
+void MainCheck()//SST main & Time keeper
 {
-  hr=hour();
-  mt=minute();
-  CurTime= (hr*60)+ mt;
-  char CurTime24[] = "00:00";
-  Dusk2Dawn::min2str(CurTime24, CurTime);
-  Blynk.virtualWrite(Timep, CurTime24 );
-  
-  if((CurTime>=SSTime)&&(SSTcheck==5))//Sunset checker
+  if(!Blynk.connected() && !pflag)//Restart at No Internet
   {
-   SSTmain(); 
-   SSTcheck=7;
-   Blynk.virtualWrite(SSTp,7);
-  }
+    
+    if(!sflag)
+    {
+      Serial.println("No internet! Seeding RTC");
+      setTime(int(EEPROM.read(0)),int(EEPROM.read(1)),0,int(EEPROM.read(2)),int(EEPROM.read(3)),2022);
+      Shr=EEPROM.read(4);
+      Smin=EEPROM.read(5);
+      SSTime=(Shr*60)+Smin;
+      Serial.println(SSTime);
+    
+      sflag=1;
+    } 
+    hr=hour();
+    mt=minute();
+    CurTime= (hr*60)+ mt;
+    EEPROM.write(0,hour());
+    EEPROM.write(1,minute());
+    EEPROM.write(2,day());
+    EEPROM.write(3,month());
+    EEPROM.commit();
 
-  if((CurTime>=SRTime)&&(SRTcount<5))//Sunrise checker
-  {
-   SRTcrow();
-   Blynk.virtualWrite(SRTp,SRTcount);
+    if(CurTime>=SSTime)
+    digitalWrite(D5,LOW);
+ 
+    if(CurTime>=GNTime)
+    digitalWrite(D5,HIGH);
   }
-
-  if((CurTime>=GNTime)&&(GN==7))//GN checker (GN Time defined in variables)
+  else
   {
-   GNlight();
-  }
+    EEPROM.write(0,hour());
+    EEPROM.write(1,minute());
+    EEPROM.write(2,day());
+    EEPROM.write(3,month());
+    EEPROM.commit();
+    hr=hour();
+    mt=minute();
+    CurTime= (hr*60)+ mt;
+    char CurTime24[] = "00:00";
+    Dusk2Dawn::min2str(CurTime24, CurTime);
+    Blynk.virtualWrite(Timep, CurTime24 );
+    
+    if((CurTime>=SSTime)&&(SSTcheck==1))//Sunset checker
+    {
+     SSTmain(); 
+     SSTcheck=0;
+     Blynk.virtualWrite(SSTp,SSTcheck);
+    }
   
-  if((hr<1)&&(SSTcheck==7))//get sunset time for NEW day and Reset values
-  {
-    SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
-    SRTime= Home.sunrise(year(), month(), day(), false)+30-10;// -10 for first light
-    SRTcount=0;
-    Blynk.virtualWrite(SRTp,SRTcount);//Reset Crow Count
-    WeatherComp=0;
-    Blynk.virtualWrite(WeaConp,WeatherComp);//Reset Weather Compensation pin
-    GN=7;// Reset GNp
-    Blynk.virtualWrite(GNp,GN);
-    SSTcheck=5;
-    char SSTime24[] = "00:00";
-    Dusk2Dawn::min2str(SSTime24, SSTime);
-    Blynk.virtualWrite(SSTp,5);
-    Blynk.virtualWrite(SSTimeCheck,SSTime24);//To get Sunset time on app
-    DisCount=0; //Reset disconnection counter to 0 at the start of new day
-    Blynk.virtualWrite(DisCounterp,DisCount);
+    if((CurTime>=SRTime)&&(SRTcount<5))//Sunrise checker
+    {
+     SRTcrow();
+     Blynk.virtualWrite(SRTp,SRTcount);
+    }
+  
+    if((CurTime>=GNTime)&&(GN==0))//GN checker (GN Time defined in variables)
+    {
+     GNlight();
+    }
+    
+    if((hr<1)&&(SSTcheck==0))//get sunset time for NEW day and Reset values
+    {
+      SSTime= Home.sunset(year(), month(), day(), false)+30+10;//30 for time zone and 10 for approx last light
+      SRTime= Home.sunrise(year(), month(), day(), false)+30-10;// -10 for first light
+      SRTcount=0;
+      Blynk.virtualWrite(SRTp,SRTcount);//Reset Crow Count
+      WeatherComp=0;
+      Blynk.virtualWrite(WeaConp,WeatherComp);//Reset Weather Compensation pin
+      GN=0;// Reset GNpin
+      Blynk.virtualWrite(GNp,GN);
+      SSTcheck=1;
+      char SSTime24[] = "00:00";
+      Dusk2Dawn::min2str(SSTime24, SSTime);
+      Blynk.virtualWrite(SSTp,SSTcheck);
+      Blynk.virtualWrite(SSTimeCheck,SSTime24);//To get Sunset time on app
+      DisCount=0; //Reset disconnection counter to 0 at the start of new day
+      Blynk.virtualWrite(DisCounterp,DisCount);
+      EEPROM.write(4,SSTime/60);
+      EEPROM.write(5,SSTime%60);
+      EEPROM.commit();
+    }
   }
 }
 
@@ -256,14 +306,14 @@ void SRTcrow()
 BLYNK_WRITE(RARp)//RAR main
 {
   RAR=param.asInt();
-  if((RAR==5)&&(RARset==1))
+  if((RAR==1)&&(RARset==1))
   {
     hr=hour();
     mt=minute();
     CurTime= (hr*60)+ mt;
     char CurTime24[] = "00:00";
     Dusk2Dawn::min2str(CurTime24, CurTime);
-    Blynk.notify("Looks like it's gonna rain at your home (Time: "+ String(CurTime24) + ")");
+    Blynk.logEvent("rain",String("Looks like it's gonna rain at your home (Time: ")+ String(CurTime24) + ")");
     for(int k=0;k<5;k++)
     {
         tone(D8,4000);
@@ -303,7 +353,7 @@ BLYNK_WRITE(Buzztestp)
 
 //////////////////////Welcome Back Home/////////////////////////
 
-BLYNK_WRITE(WBHp)//WBH main
+/*BLYNK_WRITE(WBHp)//WBH main
 {
   WBH=param.asInt();
   if((WBH==1)&&(WBHset==1))
@@ -334,7 +384,7 @@ BLYNK_WRITE(WBHsetp)//WBH switch
 BLYNK_WRITE(WBHselp)//WBH selection
 {
   WBHsel=param.asInt();
-}
+}*/
 
 ////////////////////////////////////////////////////////////////
 
@@ -356,9 +406,9 @@ void GNlight()//GN main
    if(GNset)
    {
     Blynk.virtualWrite(Sitoutp,0);
-    Blynk.notify("Good night! Sit out light has been turned OFF");
+    Blynk.logEvent("lights","Good night! Sit out light has been turned OFF");
     digitalWrite(D5,HIGH);
-    GN=5;
+    GN=1;
     Blynk.virtualWrite(GNp,GN);
    }
 }
@@ -368,11 +418,12 @@ void GNlight()//GN main
 //////////PING TEST///////////
 
 BLYNK_WRITE(Pingp)
-{int ping=param.asInt();
-if(ping==1)
-{ 
- Blynk.notify("Ping success!");
-}  
+{
+  int ping=param.asInt();
+  if(ping)
+  { 
+    Blynk.logEvent("internet","Ping success!");
+  }  
 }
   
 //////////////////////////////
@@ -390,17 +441,30 @@ BLYNK_WRITE(DisCounterp){
 ///////////////////////////////////////////////
 
 BLYNK_CONNECTED()
-{ if(pstat==0)
+{ if(!pstat)
   {
   rtc.begin();
-  Blynk.notify("I'm ready :-)");
-  delay(5000);
-  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,WeaConp,SSTp,SSTsetp,SSTimeCheck,RARsetp,WBHsetp,WBHselp,GNsetp,GNp,Buzsetp,DisCounterp,SRTp);
-  pstat++;
+  delay(1000);
+  Blynk.syncVirtual(Gatep,Sitoutp,MSRp,WeaConp,SSTp,SSTsetp,SSTimeCheck,RARsetp,/*WBHsetp,WBHselp,*/GNsetp,GNp,Buzsetp,DisCounterp,SRTp);
+  pstat=1;
+  pflag=1;//for Restart at No Internet
   digitalWrite(D4,HIGH);
+  digitalWrite(D0,HIGH);
+  tone(D8,4000);//Connection buzzer
+  delay(150);
+  noTone(D8);
+  delay(80);
+  tone(D8,4000);
+  delay(150);
+  noTone(D8);
+  delay(80);
+  tone(D8,4000);
+  delay(150);
+  noTone(D8);
   }
 else{
- if(conbuzstat==1){
+ if(conbuzstat==1)
+ {
  tone(D8,4000);//Reconnection buzzer
  delay(150);
  noTone(D8);
@@ -412,9 +476,11 @@ else{
  tone(D8,4000);
  delay(1500);
  noTone(D8);
- Blynk.notify("I've Reconnected");} 
-Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,WBHsetp,WBHselp,GNsetp,GNp,Buzsetp);
+ Blynk.logEvent("internet","I've Reconnected");
+ } 
+Blynk.syncVirtual(Gatep,Sitoutp,SSTsetp,SSTp,RARsetp,/*WBHsetp,WBHselp,*/GNsetp,GNp,Buzsetp);
 digitalWrite(D4,HIGH);
+digitalWrite(D0,HIGH);
 conbuz=0;
 DisCount++;
 Blynk.virtualWrite(DisCounterp,DisCount); //Count no. of disconnections in  a day
@@ -425,17 +491,25 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println();
-  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.7.7(b) for i-WSS-");
+  Serial.println("               -丂𝐞𝐧𝐬𝐞 𝐎𝐒 v1.8.4 for i-WSS-");
   Serial.println("Booting up...");
   pinMode(D4,OUTPUT);//Noconnection LED
-  digitalWrite(D4,LOW);
+  pinMode(D0,OUTPUT);
+  EEPROM.begin(512);
   OTA();
-  Blynk.begin(auth, ssid, pass,IPAddress(188,166,206,43),8080);
-  setSyncInterval(120 * 60);// 2 hr
-  timer.setInterval(30*1000, SSTCheck);//30 seconds
+  Blynk.connectWiFi(ssid, pass);
+  Blynk.config(auth,IPAddress(68,183,87,221),8080);
+  Blynk.connect(5);
+  setSyncInterval(30 * 60);// 30minutes
+  timer.setInterval(30*1000, MainCheck);//30 seconds
   pinMode(D3,OUTPUT);//gate
   pinMode(D5,OUTPUT);//sitout
-  delay(2000);
+  delay(3000);
+  if(!Blynk.connected())
+  {
+    digitalWrite(D5,HIGH);
+    digitalWrite(D3,HIGH);
+  }
 }
 
 void loop()
@@ -446,6 +520,7 @@ void loop()
   if (!Blynk.connected()) 
   {  
     digitalWrite(D4,LOW);
+    digitalWrite(D0,LOW);
     if((conbuz==0)&&(conbuzstat==1))
     {
         for(int i=0;i<5;i++)
